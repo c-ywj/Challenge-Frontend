a736d13913f73c940df581d584848e27d3c1536d
cy <cybc00@hotmail.com>
2017-06-23 16:38:34 -0400

    Now renders data from random user api

--- public/js/main.js
+++ public/js/main.js
@@ -2,20 +2,63 @@
 
 $( () => {
 
+  const renderMembers = function(names, photos, emails){
+    return `
+      <div class="row">
+        <div class="col s12 m7">
+          <div class="card small">
+            <div class="card-image">
+              <img src="${photos[0]}">
+            </div>
+            <div class="card-content">
+              <p>${names[0]}</p>
+              <p>${emails[0]}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  };
+
   const $loadMore = $("#js-btn-action-more");
 
+
   $loadMore.on("click", (e) => {
+    // const $content = $("#content");
     console.log("clicked");
     e.preventDefault();
     $.ajax({
-      url: 'https://randomuser.me/api/',
+      url: 'https://randomuser.me/api/?results=5',
       dataType: 'json',
       success: function(data) {
       console.log(data);
+      const firstNames = [];
+      const photos = [];
+      const emails = [];
+
+      data.results.forEach((m) => {
+        console.log(m.name.first);
+        firstNames.push(m.name.first);
+      });
+
+      data.results.forEach((m) => {
+        console.log(m.picture.thumbnail);
+        photos.push(m.picture.thumbnail);
+      });
+
+      data.results.forEach((m) => {
+        console.log(m.email);
+        emails.push(m.email);
+      });
+
+      console.log("this is the list: " + firstNames);
+      const resultTable = renderMembers(firstNames, photos, emails);
+      console.log("this is result table" + resultTable);
+
+      $("#members").html(resultTable);
+      console.log($("#members").html());
       }
     });
   });
-
-
 
 });
